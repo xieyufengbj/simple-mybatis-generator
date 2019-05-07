d5e6b581352b4047e685c77cc638dee8b25c17c8
XieYufeng <xieyufeng@mimidai.com>
2019-05-07 14:45:07 +0800

    add file about mybatis-generator

---
+++ src/main/java/com/variflight/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.variflight.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author XieYufeng
+ * @ClassName: BaseEntity
+ * @description:
+ * @date 2019/5/7 10:21
+ */
+public class BaseEntity implements Serializable {
+
+}
---
+++ src/test/java/com/variflight/MybatisSpringTest.java
@@ -0,0 +1,57 @@
+package com.variflight;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author XieYufeng
+ * @ClassName: MybatisSpringTest
+ * @description:
+ * @date 2019/5/7 12:05
+ */
+public class MybatisSpringTest {
+
+    @Test
+    public void mybatisGeneratorTest() {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        String genCfg = "generatorConfig.xml";
+        File configFile = new File(getClass().getClassLoader().getResource(genCfg).getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
